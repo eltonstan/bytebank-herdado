55fcb302d58adc97d0775d9e67c1ea5e997470bc
Elton Standerski <eltonstand@gmail.com>
2020-08-01 11:47:26 -0300

    Aula 01 a 03: Herança, polimorfismo

---
+++ src/TesteReferencias.java
@@ -0,0 +1,31 @@
+public class TesteReferencias {
+
+	public static void main(String[] args) {
+
+		Gerente g1 = new Gerente();
+		g1.setNome("Marcos");
+		g1.setSalario(5000.0);
+		System.out.println(g1.getBonificacao());
+
+		Funcionario f = new Funcionario();
+		f.setSalario(2000.0);
+		System.out.println(f.getBonificacao());
+
+		EditorVideo ev = new EditorVideo();
+		ev.setSalario(2500.0);
+		System.out.println(ev.getBonificacao());
+		
+		Designer d = new Designer();
+		d.setSalario(2000.0);
+		System.out.println(d.getBonificacao());
+
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f);
+		controle.registra(ev);
+		controle.registra(d);
+
+		System.out.println(controle.getSoma());
+
+	}
+}
